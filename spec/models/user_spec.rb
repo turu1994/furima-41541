4527d95b31b9f0d175d95ef7e881c7a6750ae118
turu1994 <katubasa1994@gmail.com>
2025-02-03 21:11:42 +0900

    テストコード追記
    ユーザー登録できるコードの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,10 @@ RSpec.describe User, type: :model do
     end
   
     describe 'ユーザー新規登録' do
+      it '全ての項目が適切に入力されていれば登録できる' do
+        expect(@user).to be_valid
+      end
+      
       it 'nicknameが空では登録できない' do
         @user.nickname = ''
         @user.valid?
