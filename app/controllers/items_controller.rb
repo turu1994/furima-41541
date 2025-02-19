28ca12217667daf7f65ef771d2ff59fd26fd3a01
turu1994 <katubasa1994@gmail.com>
2025-02-19 19:51:34 +0900

    ビュー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_fee_id, :location_id, :shipping_day_id, :price)
