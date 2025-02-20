bb6372778ae1f9647cd9f16f9c2adc545805f4ac
turu1994 <katubasa1994@gmail.com>
2025-02-20 23:56:11 +0900

    コントローラー

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :show, :update]  
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :redirect_unless_owner, only: [:edit, :update]
+
   def index
     @items = Item.all.order(created_at: :desc)
   end 
@@ -21,8 +24,28 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    if @item.update(item_params)  
+      redirect_to item_path(@item), notice: "商品情報を更新しました"
+    else
+      render :edit, status: :unprocessable_entity  
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_fee_id, :location_id, :shipping_day_id, :price)
   end
+
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def redirect_unless_owner
+    unless current_user.id == @item.user_id
+      redirect_to root_path, alert: "編集権限がありません。"
+    end
+  end
+  
 end
