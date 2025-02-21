fe539abfbd149242e9bbdbed0a5f0dc4115841f2
turu1994 <katubasa1994@gmail.com>
2025-02-21 19:20:13 +0900

    Revert "Reapply "Merge pull request #19 from turu1994/revert-18-商品情報編集機能""
    This reverts commit dc6338a4235bd7fd55072610e397b867c806d141.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :redirect_unless_owner, only: [:edit, :update]
 
+
   def index
     @items = Item.all.order(created_at: :desc)
   end 
@@ -21,7 +22,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)  
+      redirect_to item_path(@item), notice: "商品情報を更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def update
@@ -37,15 +48,19 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_fee_id, :location_id, :shipping_day_id, :price)
   end
 
-
   def set_item
     @item = Item.find(params[:id])
   end
 
   def redirect_unless_owner
+    if @item.user_id != current_user.id
+      redirect_to root_path, alert: "この商品は編集できません。"
+    end
+  end
     unless current_user.id == @item.user_id
       redirect_to root_path, alert: "編集権限がありません。"
     end
   end
   
+
 end
