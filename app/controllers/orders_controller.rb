d0deb115dffc85aa3b29d665938e2df3dc153df7
turu1994 <katubasa1994@gmail.com>
2025-02-23 10:34:53 +0900

    コントローラーの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,25 @@
 class OrdersController < ApplicationController
   def index
+    @order = Order.new
   end
   
   def create
+    @order = Order.new(order_params)
+    if @order.save
+      Address.create(address_params)
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
+  private
+
+  def order_params
+    params.permit(:user_id, :item_id).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def address_params
+    params.permit(:postal_code, :location_id, :city, :address, :building, :phone).merge(order_id: @order.id)
+  end
 end
