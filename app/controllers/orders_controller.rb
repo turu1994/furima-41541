8d164bf16ce19df5bf9a6541d0e7ae7b0bc9e5da
turu1994 <katubasa1994@gmail.com>
2025-02-24 19:57:42 +0900

    クレジットカード決済機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_address = OrderAddress.new
     @item = Item.find(params[:item_id]) 
   end
@@ -7,10 +8,12 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     @item = Item.find(params[:item_id]) 
-    if  @order_address.valid?
+    if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
@@ -18,10 +21,17 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.permit(:user_id, :item_id).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(:postal_code, :location_id, :city, :address, :building, :phone, :token)
+          .merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+  
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,  
+      card: order_params[:token],  
+      currency: 'jpy'
+    )
   end
 
-  def address_params
-    params.permit(:postal_code, :location_id, :city, :address, :building, :phone).merge(order_id: @order.id)
-  end
 end
